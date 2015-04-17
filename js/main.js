9fb2a8604c174fc1bbfe3a95843653be77312906
Tuukka Toijonen <tuukka.toijonen@gmail.com>
2015-04-17 22:14:50 +0300

    Code refactoring

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // RequestAnimFrame: a browser API for getting smooth animations
 window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame       || 
-		window.webkitRequestAnimationFrame || 
-		window.mozRequestAnimationFrame    || 
-		window.oRequestAnimationFrame      || 
-		window.msRequestAnimationFrame     ||  
+	return  window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		window.oRequestAnimationFrame      ||
+		window.msRequestAnimationFrame     ||
 		function( callback ){
 			return window.setTimeout(callback, 1000 / 60);
 		};
@@ -22,328 +22,157 @@ window.cancelRequestAnimFrame = ( function() {
 
 // Initialize canvas and required variables
 var canvas = document.getElementById("canvas"),
-		ctx = canvas.getContext("2d"), // Create canvas context
-		W = window.innerWidth / 2, // Window's width
-		H = window.innerHeight / 2, // Window's height
-		particles = [], // Array containing particles
-		ball = {}, // Ball object
-		paddles = [2], // Array containing two paddles
-		mouse = {}, // Mouse object to store it's current position
-		points = 0, // Varialbe to store points
-		fps = 60, // Max FPS (frames per second)
-		particlesCount = 20, // Number of sparks when ball strikes the paddle
-		flag = 0, // Flag variable which is changed on collision
-		particlePos = {}, // Object to contain the position of collision 
-		multipler = 1, // Varialbe to control the direction of sparks
-		startBtn = {}, // Start button object
-		box = {}, //Test box
-		boxRemoved = false,
-		restartBtn = {}, // Restart button object
-		over = 0, // flag varialbe, cahnged when the game is over
-		init, // variable to initialize animation
-		paddleHit;
+	ctx = canvas.getContext("2d"), // Create canvas context
+	W = window.innerWidth / 2, // Window's width
+	H = window.innerHeight / 2, // Window's height
+	ball, // Ball object
+	paddle, // Paddle object
+	boxes = [], // Array for boxes
+	mouse = {}, // Mouse object to store it's current position
+	points = 0, // Varialbe to store points
+	fps = 60; // Max FPS (frames per second)
 
 // Add mousemove and mousedown events to the canvas
-canvas.addEventListener("mousemove", trackPosition, true);
-canvas.addEventListener("mousedown", btnClick, true);
+document.addEventListener("mousemove", trackMouse, true);
+document.addEventListener("mousedown", mouseClick, true);
 
-// Initialise the collision sound
-collision = document.getElementById("collide");
+// Track the position of mouse cursor
+function trackMouse(e) {
+	mouse.x = e.pageX;
+	mouse.y = e.pageY;
+}
+
+// On button click (Restart and start)
+function mouseClick(e) {
+	// Variables for storing mouse position on click
+	var mx = e.pageX,
+		my = e.pageY;
+}
 
 // Set the canvas's height and width to full screen
 canvas.width = W;
 canvas.height = H;
 
 // Function to paint canvas
-function paintCanvas() {
+function clearCanvas() {
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, W, H);
 }
 
-// Function for creating paddles
-function Paddle(pos) {
-	// Height and width
-	this.h = 5;
-	this.w = 150;
-	
-	// Paddle's position
-	this.x = W/2 - this.w/2;
-	this.y = (pos == "top") ? 0 : H - this.h;
-	
+function Paddle() {
+	this.width = 150;
+	this.height = 15;
+
+	this.x = W / 2;
+	this.y = H - 15;
+
+	this.update = function() {
+		if (mouse.x) {
+			this.x = mouse.x;
+		}
+	},
+
+	this.draw = function() {
+		ctx.fillStyle = "white";
+		ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+	}
 }
 
-// Push two new paddles into the paddles[] array
-paddles.push(new Paddle("bottom"));
-//paddles.push(new Paddle("top"));
-
 // Make a collidable box
+function Box(x, y) {
 
-box = {
-	w: 300,
-	h: 15, 
-	x: W/2,
-	y: H/2,
-	
-	draw: function() {
-		ctx.strokeStyle = "red";
-		ctx.lineWidth = "2";
-		ctx.strokeRect(this.x, this.y, this.w, this.h);
-		
+	this.width = 300;
+	this.height = 15;
+
+	this.x = x;
+	this.y = y,
+
+	this.draw = function() {
+		ctx.fillStyle = "red";
+		ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
 	}
-};
+
+}
 
 // Ball object
-ball = {
-	x: 50,
-	y: 50, 
-	r: 5,
-	c: "white",
-	vx: 4,
-	vy: 8,
-	
+function Ball() {
+	this.x = 50;
+	this.y = 50;
+	this.r = 5;
+	this.c = "white";
+	this.vx = 4;
+	this.vy = 8;
+
+	// Function for updating the position of the ball
+	this.update = function() {
+		this.x+= this.vx;
+		this.y+= this.vy;
+	}
+
 	// Function for drawing ball on canvas
-	draw: function() {
+	this.draw = function() {
 		ctx.beginPath();
 		ctx.fillStyle = this.c;
 		ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
 		ctx.fill();
 	}
-};
 
+	// Function for drawing ball on canvas
+	this.testCollisions = function() {
+		if (this.x > W || this.x < 0) {
+			this.vx = -this.vx;
+		}
 
-// Start Button object
-startBtn = {
-	w: 100,
-	h: 50,
-	x: W/2 - 50,
-	y: H/2 - 25,
-	
-	draw: function() {
-		ctx.strokeStyle = "white";
-		ctx.lineWidth = "2";
-		ctx.strokeRect(this.x, this.y, this.w, this.h);
-		
-		ctx.font = "18px Arial, sans-serif";
-		ctx.textAlign = "center";
-		ctx.textBaseline = "middle";
-		ctx.fillStlye = "white";
-		ctx.fillText("Start", W/2, H/2 );
-	}
-};
+		if (this.y < 0) {
+			this.vy = -this.vy;
+		}
 
-// Restart Button object
-restartBtn = {
-	w: 100,
-	h: 50,
-	x: W/2 - 50,
-	y: H/2 - 50,
-	
-	draw: function() {
-		ctx.strokeStyle = "white";
-		ctx.lineWidth = "2";
-		ctx.strokeRect(this.x, this.y, this.w, this.h);
-		
-		ctx.font = "18px Arial, sans-serif";
-		ctx.textAlign = "center";
-		ctx.textBaseline = "middle";
-		ctx.fillStlye = "white";
-		ctx.fillText("Restart", W/2, H/2 - 25 );
-	}
-};
+		if (this.y > H) {
 
-// Function for creating particles object
-function createParticles(x, y, m) {
-	this.x = x || 0;
-	this.y = y || 0;
-	
-	this.radius = 1.2;
-	
-	this.vx = -1.5 + Math.random()*3;
-	this.vy = m * Math.random()*1.5;
-}
+		}
 
-// Draw everything on canvas
-function draw() {
-	paintCanvas();
-	for(var i = 0; i < paddles.length; i++) {
-		p = paddles[i];
-		
-		ctx.fillStyle = "white";
-		ctx.fillRect(p.x, p.y, p.w, p.h);
-	}
-	
-	ball.draw();
-	if (boxRemoved == false){
-		box.draw();
-	} else {};
-	update();
-}
+		if (this.y >= paddle.y - paddle.height / 2 && this.y <= paddle.y + paddle.height / 2) {
+			if (this.x >= paddle.x - paddle.width / 2 && this.x <= paddle.x + paddle.width / 2) {
+				this.vy = -this.vy;
+			}
+		}
 
-// Function to increase speed after every 5 points
-function increaseSpd() {
-	if(points % 4 == 0) {
-		if(Math.abs(ball.vx) < 15) {
-			ball.vx += (ball.vx < 0) ? -1 : 1;
-			ball.vy += (ball.vy < 0) ? -2 : 2;
+		for (var i = 0; i < boxes.length; i++) {
+			box = boxes[i];
+
+			if (this.y >= box.y - box.height / 2 && this.y <= box.y + box.height / 2) {
+				if (this.x >= box.x - box.width / 2 && this.x <= box.x + box.width / 2) {
+					this.vy = -this.vy;
+				}
+			}
 		}
 	}
-}
-
-// Track the position of mouse cursor
-function trackPosition(e) {
-	mouse.x = e.pageX;
-	mouse.y = e.pageY;
 }
 
 // Function to update positions, score and everything.
 // Basically, the main game logic is defined here
-function update() {
-	
+function loop() {
+
+	clearCanvas();
+
 	// Update scores
-	updateScore(); 
-	
-	// Move the paddles on mouse move
-	if(mouse.x && mouse.y) {
-		for(var i = 1; i < paddles.length; i++) {
-			p = paddles[i];
-			p.x = mouse.x - p.w/2;
-		}		
-	}
-	
-	// Move the ball
-	ball.x += ball.vx;
-	ball.y += ball.vy;
-	
-	// Collision with paddles
-	p1 = paddles[1];
-	//p2 = paddles[2];
-	
-	// If the ball strikes with paddles,
-	// invert the y-velocity vector of ball,
-	// increment the points, play the collision sound,
-	// save collision's position so that sparks can be
-	// emitted from that position, set the flag variable,
-	// and change the multiplier
-	if(collides(ball, p1)) {
-		collideAction(ball, p1);
-	}
+	updateScore();
 
-	//Collision with box
+	// Update positions
+	paddle.update();
+	ball.update();
+	ball.testCollisions();
 
-	else if(collides(ball, box)) {
-		collideAction(ball, box);
-	}
-	
-	
-	else {
-		// Collide with walls, If the ball hits the top/bottom,
-		// walls, run gameOver() function
-		if(ball.y + ball.r > H) {
-			ball.y = H - ball.r;
-			gameOver();
-		} 
-		
-		else if(ball.y < 1) {
-			ball.vy = -ball.vy;
-			//ball.vx = -ball.vx;
-		}
-		
-		// If ball strikes the vertical walls, invert the 
-		// x-velocity vector of ball
-		if(ball.x + ball.r > W) {
-			ball.vx = -ball.vx;
-			ball.x = W - ball.r;
-		}
-		
-		else if(ball.x -ball.r < 0) {
-			ball.vx = -ball.vx;
-			ball.x = ball.r;
-		}
-	}
-	
-	
-	
-	// If flag is set, push the particles
-	if(flag == 1) { 
-		for(var k = 0; k < particlesCount; k++) {
-			particles.push(new createParticles(particlePos.x, particlePos.y, multiplier));
-		}
-	}	
-	
-	// Emit particles/sparks
-	emitParticles();
-	
-	// reset flag
-	flag = 0;
-}
+	// Draw stuff on canvas
+	paddle.draw();
+	ball.draw();
 
-//Function to check collision between ball and one of
-//the paddles. This probably needs to be rewritten in order to make collisions with boxes
-function collides(b, p) {
-	if(b.x + ball.r >= p.x && b.x - ball.r <=p.x + p.w) {
-		if(b.y >= (p.y - p.h) && p.y < 400){
-			//b.y >= (p.y - p.h) && (p.y > 0 && p.y < 600)
-			boxHit = 1;
-			return true;
-		}
+	for (var i = 0; i < boxes.length; i++) {
+		boxes[i].draw();
+	};
 
-		else if(b.y >= (p.y - p.h) && p.y > 550 ){
-			paddleHit = 1;
-			return true;
-		}
-		
-		else return false;
-	}
-}
+	requestAnimFrame(loop);
 
-//Do this when collides == true
-function collideAction(ball, p) {
-	ball.vy = -ball.vy;
-	
-	if(boxHit == 1){
-		particlePos.y = ball.y + ball.r;
-		boxRemoved = true;
-	}
-	else if(paddleHit == 1) {
-		ball.y = p.y - p.h;
-		particlePos.y = ball.y + ball.r;
-		multiplier = -1;	
-	}
-	
-	points++;
-	increaseSpd();
-	
-	if(collision) {
-		if(points > 0) 
-			collision.pause();
-		
-		collision.currentTime = 0;
-		collision.play();
-	}
-	
-	particlePos.x = ball.x;
-	flag = 1;
-}
-
-// Function for emitting particles
-function emitParticles() { 
-	for(var j = 0; j < particles.length; j++) {
-		par = particles[j];
-		
-		ctx.beginPath(); 
-		ctx.fillStyle = "white";
-		if (par.radius > 0) {
-			ctx.arc(par.x, par.y, par.radius, 0, Math.PI*2, false);
-		}
-		ctx.fill();	 
-		
-		par.x += par.vx; 
-		par.y += par.vy; 
-		
-		// Reduce radius so that the particles die after a few seconds
-		par.radius = Math.max(par.radius - 0.05, 0.0); 
-		
-	} 
 }
 
 // Function for updating score
@@ -355,65 +184,17 @@ function updateScore() {
 	ctx.fillText("Score: " + points, 20, 20 );
 }
 
-// Function to run when the game overs
-function gameOver() {
-	ctx.fillStlye = "white";
-	ctx.font = "20px Arial, sans-serif";
-	ctx.textAlign = "center";
-	ctx.textBaseline = "middle";
-	ctx.fillText("Game Over - You scored "+points+" points!", W/2, H/2 + 25 );
-	
-	// Stop the Animation
-	cancelRequestAnimFrame(init);
-	
-	// Set the over flag
-	over = 1;
-	
-	// Show the restart button
-	restartBtn.draw();
+function initGame() {
+
+	paddle = new Paddle();
+	ball = new Ball();
+
+	for (var i = 0; i < 5; i++) {
+		boxes.push(new Box(i * 100, i * 25));
+	};
+
+	requestAnimFrame(loop);
+
 }
 
-// Function for running the whole animation
-function animloop() {
-	init = requestAnimFrame(animloop);
-	draw();
-}
-
-// Function to execute at startup
-function startScreen() {
-	draw();
-	startBtn.draw();
-}
-
-// On button click (Restart and start)
-function btnClick(e) {
-	
-	// Variables for storing mouse position on click
-	var mx = e.pageX,
-			my = e.pageY;
-	
-	// Click start button
-	if(mx >= startBtn.x && mx <= startBtn.x + startBtn.w) {
-		animloop();
-		
-		// Delete the start button after clicking it
-		startBtn = {};
-	}
-	
-	// If the game is over, and the restart button is clicked
-	if(over == 1) {
-		if(mx >= restartBtn.x && mx <= restartBtn.x + restartBtn.w) {
-			ball.x = 20;
-			ball.y = 20;
-			points = 0;
-			ball.vx = 4;
-			ball.vy = 8;
-			animloop();
-			
-			over = 0;
-		}
-	}
-}
-
-// Show the start screen
-startScreen();
+initGame();
